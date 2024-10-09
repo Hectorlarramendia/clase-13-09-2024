43c353103f5cf22b2065e43d914bedf4b722279a
Hectorlarramendia <hectorlarramendia65@gmail.com>
2024-10-09 11:51:12 -0300

    ejerc.py

---
+++ ejer08.py
@@ -0,0 +1,18 @@
+"""_sumary_
+operaciones con cadenas
+Introduzca una cadena y muestre en miunusculas y mayusculas 
+""" 
+
+cadena = input("cadena: ") # ingresar cadena
+
+mayus = cadena.upper () # poner en mayuscula 
+minus = cadena.lower() # poner en minuscula 
+cap = cadena.capitalize() # Capitalizar
+
+print(mayus) # imprimir
+print(minus) # imprimir
+ 
+
+lista = cadena.split("o") # dividir la cadena en una lista dividida por "o"
+
+print(lista) # imprimir lista 
