77cdd71afcefafb4fa9db30ac5b35442ce2aed4b
Hectorlarramendia <hectorlarramendia65@gmail.com>
2024-11-30 21:36:43 -0300

    ejerc04.py

--- clase 29-11/ejerc04.py
+++ clase 29-11/ejerc04.py
@@ -1,19 +1,17 @@
 import math
 
+# Función para calcular el perímetro de un círculo
 def perimetro_circulo(radio):
     return 2 * math.pi * radio
 
-
-
+# Función para calcular el área de un círculo
 def area_circulo(radio):
     return math.pi * radio ** 2
+
 def main():
-    radio = float(input("radio del circulo"))
-    ancho = float(input("ancho del circulo"))
-    perimetro = perimetro_circulo(radio)
-    area = area_circulo(radio)
-    print("El perimetro del circulo es {perimetro:.2f} ")
-    print("El area de un circulo es {area:.2f}")
-    
-    if __name__ == "__main__":
-     main()
+    radio = float(input("Ingrese el radio del circulo: "))
+    print(f"El perimetro del circulo es: {perimetro_circulo(radio):.2f}")
+    print(f"El area del circulo es: {area_circulo(radio):.2f}")
+
+if __name__ == "__main__":
+    main()
