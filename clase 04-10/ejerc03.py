79e99dfa21b2bc38b7505f53bebcadcfd22f18e6
Hectorlarramendia <hectorlarramendia65@gmail.com>
2024-12-06 09:41:49 -0300

    clase 04-10

---
+++ clase 04-10/ejerc03.py
@@ -0,0 +1,21 @@
+"""
+Operaciones con cadenas
+"""
+
+cadena = "    Hola  mundo   "
+
+""" print(f"tamaño: {len(cadena)}")
+print(f"limpiar: {cadena}")
+print(f"limpiar: {cadena}")
+ """
+cadena = cadena.strip()
+""" for i in cadena:
+    print(i) """
+
+print(cadena[0])
+print(cadena[1:4])
+print(cadena[::-1])
+print(cadena[:4])
+print(cadena[7:])
+print(cadena[-4:])
+print(cadena[-4::-1])
